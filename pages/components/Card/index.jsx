10c322a9d6a89bb7b6d7a3942f60acbf5460f68d
Daryl Findlay <darylfindlay@gmail.com>
2021-03-29 23:57:59 +0100

    adding tailwind

---
+++ pages/components/Card/index.jsx
@@ -0,0 +1,13 @@
+export default function Card() {
+  return (
+    <div className="p-6 max-w-sm bg-gray-700 rounded-xl shadow-xl flex items-center space-x-4 my-2">
+      <div className="flex-shrink-0">
+        <img className="h-20 w-20 rounded-full" src="http://www.fillmurray.com/200/200" alt="ChitChat Logo" />
+      </div>
+      <div>
+        <div className="text-xl font-medium text-yellow-500">Daryl Findlay</div>
+        <p className="text-yellow-200">Welcome to my protfolio!</p>
+      </div>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,29 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Card from './components/Card';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="h-screen mx-auto bg-gray-800 pt-16">
       <Head>
-        <title>Create Next App</title>
+        <title>Daryl Findlay - Web Engineer 👨🏼‍💻</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <main className="container mx-auto rounded-xl bg-gray-300 p-8">
+        <h1 className="text-6xl font-bold tracking-wide font-mono text-red-900">Daryl's Portfolio</h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className="flex justify-between">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+        <div className="mx-auto">
+          <p className="text-xl">
+            Built with NextJS and Tailwind!
+          </p>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 }
